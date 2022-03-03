bae6d4003a180a8d5bf24c200ce25c4a825ebf1e
mohinikoli <shreyakoli2653@gmail.com>
2022-03-03 16:52:41 +0530

    footer page update

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var elements = $('.modal-overlay, .modal');
+
+    $('.menu-icon').click(function () {
+      // alert('hello');
+      elements.addClass('active');
+    });
+
+    $('.close-modal').click(function () {
+      elements.removeClass('active');
+    });
+       $('#example').DataTable();
+
+});
